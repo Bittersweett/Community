2dfa64f81bf424f1332683a8072d9ad11d670756
lijunru <403447007@qq.com>
2019-07-07 16:52:46 +0800

    检查用户是否登录才能跳转到指定URL

---
+++ src/main/java/com/nowcoder/community/annotation/LoginRequired.java
@@ -0,0 +1,13 @@
+package com.nowcoder.community.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME) //注解有效时长
+public @interface LoginRequired {
+
+
+}
