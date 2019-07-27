caf010146e4a7d93c65893503dd7a8545acef5f9
lijunru <403447007@qq.com>
2019-07-27 17:55:16 +0800

    用Kafka发送系统的通知

--- src/main/java/com/nowcoder/community/controller/FollowController.java
+++ src/main/java/com/nowcoder/community/controller/FollowController.java
@@ -1,7 +1,9 @@
 package com.nowcoder.community.controller;
 
+import com.nowcoder.community.entity.Event;
 import com.nowcoder.community.entity.Page;
 import com.nowcoder.community.entity.User;
+import com.nowcoder.community.event.EventProducer;
 import com.nowcoder.community.service.FollowService;
 import com.nowcoder.community.service.UserService;
 import com.nowcoder.community.util.CommunityConstant;
@@ -30,6 +32,8 @@ public class FollowController implements CommunityConstant {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private EventProducer eventProducer;
     /**
      * 关注
      *
@@ -43,12 +47,21 @@ public class FollowController implements CommunityConstant {
         User user = hostHolder.getUser();
 
         followService.follow(user.getId(), entityType, entityId);
+
+        //触发关注事件
+        Event event = new Event()
+                .setTopic(TOPIC_FOLLOW)
+                .setUserId(hostHolder.getUser().getId())
+                .setEntityType(entityType)
+                .setEntityId(entityId)
+                .setEntityUserId(entityId);
+        eventProducer.fireEvent(event);
+
         return CommunityUtil.getJSONString(0, "已关注!");
     }
 
     /**
      * 取消关注
-     *
      * @param entityType
      * @param entityId
      * @return
--- src/main/java/com/nowcoder/community/util/CommunityConstant.java
+++ src/main/java/com/nowcoder/community/util/CommunityConstant.java
@@ -13,4 +13,10 @@ public interface CommunityConstant {
     int ENTITY_TYPE_POST = 1;//实体类型：帖子
     int ENTITY_TYPE_COMMENT = 2;//实体类型：评论
     int ENTITY_TYPE_USER = 3; //实体类型：用户
+
+    String TOPIC_COMMENT = "comment"; //主题：评论
+    String TOPIC_LIKE = "like"; //主题：点赞
+    String TOPIC_FOLLOW = "follow"; //主题：关注
+
+    int SYSTEM_USER_ID = 1; //系统用户ID
 }
